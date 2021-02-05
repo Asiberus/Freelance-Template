e2656ec908a872e066847278c8c3d545952987e8
ArthurBeaulieu <contact@abeaulieu.fr>
2021-02-05 21:17:38 +0100

    add edit info

--- src/js/FreelanceTemplate.js
+++ src/js/FreelanceTemplate.js
@@ -30,7 +30,19 @@ class FreelanceTemplate {
     }
 
     document.querySelector('#hello-freelancer').innerHTML = `Bonjour ${info.surname}!`;
+    document.querySelector('#edit-freelancer').addEventListener('click', this._editFreelancer.bind(this));
     document.querySelector('#new-quotation').addEventListener('click', this._newQuotation.bind(this));
+  }
+
+
+  _editFreelancer() {
+    new InitModal({
+      url: 'assets/html/InitModal.html',
+      data: JSON.parse(this._ls.getItem('freelancer-info')),
+      callback: info => {
+        this._ls.setItem('freelancer-info', JSON.stringify(info));
+      }
+    });
   }
 
 
--- src/js/modal/InitModal.js
+++ src/js/modal/InitModal.js
@@ -9,6 +9,7 @@ class InitModal extends Modal {
     super(options);
 
     this._cb = options.callback;
+    this._data = options.data;
     this._saveButton = null;
     this.save = this.save.bind(this);
   }
@@ -22,6 +23,22 @@ class InitModal extends Modal {
 
   _fillAttributes() {
     this._saveButton = this._rootElement.querySelector('#modal-footer-save');
+    if (this._data) {
+      this._rootElement.querySelector('#civility').value = this._data.genre;
+      this._rootElement.querySelector('#surname').value = this._data.surname;
+      this._rootElement.querySelector('#name').value = this._data.name;
+      this._rootElement.querySelector('#address').value = this._data.address;
+      this._rootElement.querySelector('#zipcode').value = this._data.zipcode;
+      this._rootElement.querySelector('#town').value = this._data.town;
+      this._rootElement.querySelector('#phone').value = this._data.phone;
+      this._rootElement.querySelector('#mail').value = this._data.mail;
+      this._rootElement.querySelector('#siret').value = this._data.siret;
+      this._rootElement.querySelector('#ape').value = this._data.ape;
+      this._rootElement.querySelector('#tva').value = this._data.tva;
+      this._rootElement.querySelector('#bank').value = this._data.bank;
+      this._rootElement.querySelector('#iban').value = this._data.iban;
+      this._rootElement.querySelector('#bic').value = this._data.bic;
+    }
     this._events();
   }
 
